31492779bb1021b9ca2bba58c7620783d69d3eb3
Sai Kumar <pothugantisaikumar3001@gmail.com>
2026-04-08 22:33:38 +0530

    chore: keep frontend-only files

--- backend/src/main/java/com/feedback/backend/repository/FeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.feedback.backend.repository;
-
-import com.feedback.backend.entity.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-}
